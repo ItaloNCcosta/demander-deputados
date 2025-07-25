88b39be3980d2ff46722182e213f1301754bc11e
ItaloNCcosta <italonccosta@gmail.com>
2025-07-24 21:55:46 -0300

    wip

--- app/Http/Controllers/Deputy/DeputyController.php
+++ app/Http/Controllers/Deputy/DeputyController.php
@@ -1,16 +1,20 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Controllers\Deputy;
 
 use App\Enums\PartyEnum;
 use App\Enums\StateEnum;
 use App\Http\Controllers\Controller;
+use App\Models\Deputy;
 use App\Services\Deputy\DeputyListService;
 use App\Services\Deputy\DeputyRankingService;
+use App\Services\Deputy\DeputyShowService;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
-class DeputyController extends Controller
+final class DeputyController extends Controller
 {
     public function index(
         Request $request,
@@ -27,6 +31,27 @@ class DeputyController extends Controller
             'state' => $state,
             'party' => $party,
             'ranking'  => $ranking,
+        ]);
+    }
+
+    public function show(
+        Request $request,
+        Deputy $deputy,
+        DeputyShowService $showService
+    ): View {
+        $filters = $request->only([
+            'type',
+            'date_start',
+            'date_end',
+            'order_by',
+        ]);
+
+        $data = $showService->handle($deputy, $filters);
+
+        return view('deputy.show', [
+            'deputy'   => $data['deputy'],
+            'expenses' => $data['expenses'],
+            'filters'  => $filters,
         ]);
     }
 }
